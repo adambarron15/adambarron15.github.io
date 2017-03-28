6a6dc01a4955291a9cf03cdc64283ae6cbfbf7aa
adambarron15 <adambarron97@siu.edu>
2017-03-28 17:55:31 -0500

    added blah

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
- $(document).ready(function()    {
-    $('div').height("200px");
-    $('div').width("200px");
-    $('div').css("border-radius", "10px");
-    $('div').click(function(){
-      $('div').css("background-color", "red");
-    })
-});
+
+$(document).ready(function(){
+    $('ol').sortable();
+})
